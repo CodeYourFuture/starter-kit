c1a29bed21d3c39d688020cdf3c8291ea5bb133a
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-05-12 00:41:21 +0100

    Prepare CYF branch

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import http from "http";
 
 import app from "./app";
+import { connectDb } from "./db";
 
 const port = parseInt(process.env.PORT || "3000");
 
 const server = http.createServer(app);
-
-server.listen(port);
 
 server.on("listening", () => {
 	const addr = server.address();
 	const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
 	// eslint-disable-next-line no-console
 	console.log(`Listening on ${bind}`);
+});
+
+connectDb().then(() => {
+	server.listen(port);
 });
