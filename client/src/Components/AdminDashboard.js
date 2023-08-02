23299623c4dbee6fba350bd11a79c9ef4b546bf6
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-02 21:28:05 +0100

    install  npm init again

---
+++ client/src/Components/AdminDashboard.js
@@ -0,0 +1,25 @@
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+function SelectMenu() {
+  const [selectedValue, setSelectedValue] = useState("Open this select menu");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <div>
+     <Navbar />
+    <select className="form-select" aria-label="Default select example" value={selectedValue} onChange={handleChange}>
+      <option selected>Open this select menu</option>
+      <option value="1">codewars</option>
+      <option value="2">attendance</option>
+      <option value="3">codility</option>
+    </select>
+    <Footer />
+    </div>
+  );
+}
+
+export default SelectMenu;
