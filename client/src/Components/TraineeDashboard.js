59e2692657151c39c4e363e6ec982baa1f36d990
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-02 19:54:02 +0100

    add navbar and footer

--- client/src/Components/TraineeDashboard.js
+++ client/src/Components/TraineeDashboard.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
-class SelectMenu extends React.Component {
-  render() {
-    return (
-      <select className="form-select" aria-label="Default select example">
-        <option selected>Open this select menu</option>
-        <option value="1">attandance</option>
-        <option value="2">codility</option>
-        <option value="3">codwars</option>
-      </select>
-    );
-  }
+function SelectMenu() {
+  const [selectedValue, setSelectedValue] = useState("Open this select menu");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <div>
+     <Navbar />
+    <select className="form-select" aria-label="Default select example" value={selectedValue} onChange={handleChange}>
+      <option selected>Open this select menu</option>
+      <option value="1">codewars</option>
+      <option value="2">attendance</option>
+      <option value="3">codility</option>
+    </select>
+    <Footer />
+    </div>
+  );
 }
 
 export default SelectMenu;
+
