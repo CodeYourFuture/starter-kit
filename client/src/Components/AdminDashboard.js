b2a3387a02d05d1fd0cf3ccf956f1038fad08e8b
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-03 22:01:43 +0100

    for change

--- client/src/Components/AdminDashboard.js
+++ client/src/Components/AdminDashboard.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 function AdminDashboard() {
   const [selectedValue, setSelectedValue] = useState("Open this select menu");
